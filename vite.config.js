8de45e58d08605ff61df416e0ed4843f6e6a35cb
Gabriel-S4 <gabsa2300@gmail.com>
2025-03-20 13:17:31 -0300

    doublegreen V1.0

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss() ],
+  base: '/projeto-stats/', // Altere para o nome do seu repositório
 })
